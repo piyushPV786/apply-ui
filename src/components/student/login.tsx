9b5e99cd38f56aa757916edeb5f081738b8f5609
shashank gupta <shashankg@regenesys.net>
2022-12-06 18:19:42 +0530

    style refactor foir login

--- src/components/student/login.tsx
+++ src/components/student/login.tsx
@@ -1,8 +1,6 @@
-import { Grid, Input, Snackbar } from "@material-ui/core";
+import { Grid, Snackbar } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import StyledButton from "../button/button";
-import { GreenText } from "../common/common";
 import { useRouter } from "next/router";
 import OtpInput from "../Input/otpInput";
 import CheckCircleRoundedIcon from "@material-ui/icons/CheckCircleRounded";
@@ -11,6 +9,17 @@ import "react-phone-number-input/style.css";
 import axios from "./../../service/Api";
 import RBSLogo from "../../../public/assets/images/RBS_logo_1_white.svg";
 import Image from "next/image";
+import {
+  ImageContainer,
+  ApplicationFormContainer,
+  Heading,
+  Item,
+  StyleFooter,
+  StyledLink,
+  SuccessMsgContainer,
+  Title,
+  ToasterContainer,
+} from "./style";
 
 const StudentLogin = () => {
   const [mobileNumber, setMobileNumber] = useState<string>("");
@@ -214,8 +223,7 @@ const StudentLogin = () => {
       message: "OTP re-sent successfully",
     }));
     setToast(true);
-    setOtp('')
-
+    setOtp("");
   };
 
   const { message, success } = toastMsg;
@@ -279,93 +287,3 @@ const StudentLogin = () => {
 };
 
 export default StudentLogin;
-export const SuccessMsgContainer = styled.div`
-  display: flex;
-`;
-export const ToasterContainer = styled.div<any>`
-  display: flex;
-  column-gap: 10px;
-  width: 310px;
-  background: ${({ success }) => (success ? "#e6f4e7" : "#d9534f")};
-  height: 75px;
-  color: white;
-  padding: 1rem;
-  display: flex;
-  border-radius: 5px;
-  align-items: center;
-`;
-const StyleFooter = styled.div`
-  position: absolute;
-  bottom: 10px;
-  color: white;
-  a,
-  a:hover {
-    color: #fcd400;
-  }
-  @media screen and (min-width: 410px) and (max-width: 450px) {
-    bottom: 260px;
-    font-size: 14px;
-  }
-  @media screen and (min-width: 380px) and (max-width: 395px) {
-    bottom: 160px;
-    font-size: 14px;
-  }
-  @media screen and (min-width: 320px) and (max-width: 380px) {
-    bottom: 60px;
-    font-size: 12px;
-  }
-`;
-const ApplicationFormContainer = styled.div<any>`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  background: white;
-  padding: 0.8rem;
-  border-radius: 3px;
-  @media (max-width: 400px) {
-    top: ${({ isProceed }) => (isProceed ? "62%" : "55%")};
-  }
-`;
-
-const Item = styled.div`
-  text-align: center;
-  button {
-    text-align: center;
-    width: 100%;
-    margin-bottom: 0.7rem;
-  }
-`;
-
-const Title = styled(GreenText)``;
-
-const Heading = styled.span`
-  color: #fcd400;
-  font-size: 18px;
-  font-weight: bold;
-  padding: 1rem 0;
-  position: relative;
-  text-align: center;
-  top: 5%;
-`;
-
-export const StyledLink = styled.span`
-  color: #008554;
-  cursor: pointer;
-`;
-
-const url = "/assets/images/bg.jpg";
-const MainContainer = styled.div``;
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  position: fixed;
-  background-image: url(${url});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  overflow-y: scroll;
-`;
