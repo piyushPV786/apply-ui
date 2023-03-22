6b24f817aed1555c20f350c95b3dd1db4f8a0733
arun <ap@samyotech.com>
2023-03-22 15:22:20 +0530

    upload document not seeing after fees payment

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -340,6 +340,8 @@ function ApplicationCard({
   const payBtnTitle = isAcceptedApplication
     ? "Pay Program Fee"
     : "Pay Application Fee";
+
+  const showUploadBtn = status.includes(CommonEnums.APP_FEE_VER_PEND);
   return (
     <>
       <ApplicationContainer className="container bg-white p-3 app-card border rounded ">
@@ -411,6 +413,16 @@ function ApplicationCard({
                 title={payBtnTitle}
               />
             )}{" "}
+            {showUploadBtn && (
+              <StyledButton
+                onClick={() =>
+                  onUploadDocuments(applicationNumber, leadCode, true)
+                }
+                isUploadBtn
+                className="card-button"
+                title="Upload Document"
+              />
+            )}
             {showDocumentUploadBtn && (
               <StyledButton
                 onClick={() =>
