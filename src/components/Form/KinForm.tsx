fafa7336b8e98e60785b2cefcd4b36c24e55231d
shashank gupta <shashankg@regenesys.net>
2023-06-21 13:19:48 +0530

    fix bug

--- src/components/Form/KinForm.tsx
+++ src/components/Form/KinForm.tsx
@@ -51,7 +51,6 @@ export const KinDetailsForm = ({ leadId, relationData }: IKinForm) => {
   const phoneNumberVal = watch(phoneNumber);
   const isKinDetailExist = watch(KinDetails);
   const isKinNeed = isNextKinVal === "yes";
-
   const uppdateMobNumber = () => {
     if (countryCodeRef) {
       const countryCode = getCountryCallingCode(countryCodeRef);
@@ -86,10 +85,11 @@ export const KinDetailsForm = ({ leadId, relationData }: IKinForm) => {
         unregister(phoneNumber, {
           keepError: false,
           keepIsValid: true,
+          keepDefaultValue: true,
         });
       }
     }
-  }, [isKinDetailExist]);
+  }, [isKinDetailExist, error]);
 
   return (
     <>
@@ -131,116 +131,125 @@ export const KinDetailsForm = ({ leadId, relationData }: IKinForm) => {
         </AccordionSummary>
 
         <AccordionDetails hidden={!isNextKinVal || isNextKinVal === "no"}>
-          <div className="container-fluid form-padding">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="mb-4">
-                  <StyledLabel required>Full Name</StyledLabel>
-                  <input
-                    className="form-control"
-                    value={fullNameVal}
-                    defaultValue={fullNameVal}
-                    {...register(`${fullName}`, { required: isKinNeed })}
-                    onChange={(e) => {
-                      const value = e.target.value;
-                      const name = e.target.name;
-                      if (onlyAlphabets(value)) {
-                        setValue(name, value, formOptions);
-                      }
-                    }}
-                  />
-                  {error && error?.fullName && (
-                    <div className="invalid-feedback">
-                      Please enter full name
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="mb-4">
-                  <AdvanceDropDown
-                    options={relationData}
-                    label={"RelationShip"}
-                    value={relationShipVal}
-                    name={relationShip}
-                    register={register}
-                    required={isKinNeed}
-                  />
-                  {error && error?.relationship && (
-                    <div className="invalid-feedback">
-                      Please enter relationship
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="mb-4">
-                  <StyledLabel required>Email</StyledLabel>
-                  <input
-                    className="form-control"
-                    value={EmailVal}
-                    defaultValue={EmailVal}
-                    {...register(`${Email}`, {
-                      required: isKinNeed,
-                      validate: (value) => isValidEmail(value, !isKinNeed),
-                    })}
-                  />
-                  {error && error?.email && (
-                    <div className="invalid-feedback">
-                      {error?.email?.type == "validate"
-                        ? "you have entered an invalid email address. Please try again"
-                        : "Please enter email"}
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-4">
-                <div className="mb-4">
-                  <StyledLabel required>Mobile Number</StyledLabel>
-                  <Controller
-                    control={control}
-                    name={phoneNumber}
-                    rules={{
-                      required: isKinNeed,
-                      validate: (value) =>
-                        validateNumber(value, countryCodeRef) ||
-                        "Invalid phone number",
-                    }}
-                    render={({ field }) => (
-                      <PhoneInput
-                        {...field}
-                        id="2"
-                        international
-                        countryCallingCodeEditable={false}
-                        defaultCountry={countryCodeRef}
-                        placeholder="Select Country Code*"
-                        onCountryChange={(value: any) => {
-                          setCountryCode(value);
-                        }}
-                        onBlur={() => {
-                          field.onBlur();
-                          uppdateMobNumber();
-                        }}
-                        onChange={(value) => {
-                          field.onChange(value);
-                        }}
-                        value={field.value}
-                      />
+          {(isNextKinVal || isNextKinVal !== "no") && (
+            <div className="container-fluid form-padding">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="mb-4">
+                    <StyledLabel required>Full Name</StyledLabel>
+                    <input
+                      className="form-control"
+                      value={fullNameVal}
+                      defaultValue={fullNameVal}
+                      {...register(`${fullName}`, { required: isKinNeed })}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        const name = e.target.name;
+                        if (onlyAlphabets(value)) {
+                          setValue(name, value, formOptions);
+                        }
+                      }}
+                    />
+                    {error && error?.fullName && (
+                      <div className="invalid-feedback">
+                        Please enter full name
+                      </div>
                     )}
-                  />
-                  {error && error?.mobileNumber && (
-                    <div className="invalid-feedback">
-                      {error?.mobileNumber.type === "validate"
-                        ? "you have entered an invalid number"
-                        : " Please enter phone number"}
-                    </div>
-                  )}
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="mb-4">
+                    <AdvanceDropDown
+                      options={relationData}
+                      label={"RelationShip"}
+                      value={relationShipVal}
+                      name={relationShip}
+                      register={register}
+                      required={isKinNeed}
+                    />
+                    {error && error?.relationship && (
+                      <div className="invalid-feedback">
+                        Please enter relationship
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="mb-4">
+                    <StyledLabel required>Email</StyledLabel>
+                    <input
+                      className="form-control"
+                      value={EmailVal}
+                      defaultValue={EmailVal}
+                      {...register(`${Email}`, {
+                        required: isKinNeed,
+                        validate: (value) => isValidEmail(value, !isKinNeed),
+                      })}
+                    />
+                    {error && error?.email && (
+                      <div className="invalid-feedback">
+                        {error?.email?.type == "validate"
+                          ? "you have entered an invalid email address. Please try again"
+                          : "Please enter email"}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="mb-4">
+                    <StyledLabel required>Mobile Number</StyledLabel>
+                    <Controller
+                      control={control}
+                      name={phoneNumber}
+                      rules={{
+                        required: isKinNeed ? true : false,
+                        validate: {
+                          validPhoneNumber: (value) => {
+                            if (isKinNeed) {
+                              return (
+                                validateNumber(value, countryCodeRef) ||
+                                "Invalid phone number"
+                              );
+                            }
+                          },
+                        },
+                      }}
+                      render={({ field }) => (
+                        <PhoneInput
+                          {...field}
+                          id="2"
+                          international
+                          countryCallingCodeEditable={false}
+                          defaultCountry={countryCodeRef}
+                          placeholder="Select Country Code*"
+                          onCountryChange={(value: any) => {
+                            setCountryCode(value);
+                          }}
+                          onBlur={() => {
+                            field.onBlur();
+                            uppdateMobNumber();
+                          }}
+                          onChange={(value) => {
+                            field.onChange(value);
+                          }}
+                          value={field.value}
+                        />
+                      )}
+                    />
+                    {error && error?.mobileNumber && (
+                      <div className="invalid-feedback">
+                        {error?.mobileNumber.type === "validate"
+                          ? "you have entered an invalid number"
+                          : " Please enter phone number"}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </AccordionDetails>
       </StyledAccordion>
     </>
