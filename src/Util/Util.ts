f5cf2e47092a1469f6c09e00974474dd879ac91e
Vedantc <vedantc@regenesys.net>
2023-06-27 20:12:06 +0530

    validation fixed ticket no 3620

--- src/Util/Util.ts
+++ src/Util/Util.ts
@@ -395,9 +395,9 @@ export const mapFormDefaultValue = (
           : studentData[key]?.agentCode
           ? "AGENT"
           : "";
-        setValue(key, value, {});
+        setValue(key, value);
       }
-      setValue(key, value, {});
+      setValue(key, value);
     }
     setValue("education.referredById", valueCode, formOptions);
   }
