cb1351bcdce8db633ddae7898dba6a18fc501efc
Vedantc <vedantc@regenesys.net>
2023-07-28 11:49:28 +0530

    merge conflict

--- src/components/Form/EducationForm.tsx
+++ src/components/Form/EducationForm.tsx
@@ -260,7 +260,7 @@ export const EducationForm = (props: IEducationProps) => {
                                   title={description}
                                   placement="top"
                                   arrow
-                                  open={true}
+                                  open={studyModeVal == studyModeCode}
                                 >
                                   <span className="form-check form-check-inline">
                                     <input
@@ -624,47 +624,6 @@ const StyleFeeCards = styled.div`
   -webkit-transition: all 0.5s;
   -ms-transition: all 0.5s;
   -o-transition: all 0.5s;
-`;
-
-const Tooltipdiv = styled.div`
-  .tooltip {
-    position: relative;
-    display: inline-block;
-    border-bottom: 1px dotted black;
-  }
-
-  .tooltip .tooltiptext {
-    visibility: hidden;
-    width: 120px;
-    background-color: #555;
-    color: #fff;
-    text-align: center;
-    border-radius: 6px;
-    padding: 5px 0;
-    position: absolute;
-    z-index: 1;
-    bottom: 125%;
-    left: 50%;
-    margin-left: -60px;
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-
-  .tooltip .tooltiptext::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: #555 transparent transparent transparent;
-  }
-
-  .tooltip:hover .tooltiptext {
-    visibility: visible;
-    opacity: 1;
-  }
 `;
 
 const StyleContainer = styled.div`
