5cd5b67e05c40000c5795ec0fabb58140d21de82
regenabhi <Abhishekl@regenesys.net>
2023-10-17 13:36:28 +0530

    services pattern

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,1 +1,8 @@
-import axios from "./index";
+import axiosInstance from "./index";
+
+class DashboardServices {
+  async getAllApplication() {
+    const url = base + "/" + sada;
+    const response = await axiosInstance.get(url);
+  }
+}
