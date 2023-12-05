810275d81abff5189383038126e196cccdcc4085
Vedantc <vedantc@regenesys.net>
2023-12-05 18:45:36 +0530

    draft api failed changes

--- src/components/lead/form/data/sponsorData.ts
+++ src/components/lead/form/data/sponsorData.ts
@@ -117,7 +117,7 @@ export const sponsorInfoData = [
     name: "zipCode",
     key: "zipCode",
     label: "Pin Code / Zip Code",
-    type: "text",
+    type: "number",
     required: false,
     errorMessage: "Please enter Pin Code / Zip Code",
     hidden: false,
