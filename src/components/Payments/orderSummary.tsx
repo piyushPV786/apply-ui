2e77e60448581497c966e7fd6f04e00fe131d591
Vedantc <vedantc@regenesys.net>
2024-01-08 18:21:46 +0530

    Discount issue fixed

--- src/components/Payments/orderSummary.tsx
+++ src/components/Payments/orderSummary.tsx
@@ -149,7 +149,7 @@ const FinalFees = (props) => {
   useEffect(() => {
     methods.setValue("discountCode", "");
   }, [fees.discountAmount]);
-
+  console.log(discount);
   return (
     <Grid container spacing={2} sx={{ p: 3 }}>
       <PaymentCard>
@@ -232,7 +232,7 @@ const FinalFees = (props) => {
               {showDiscount && (
                 <FormProvider {...methods}>
                   <form>
-                    {discount?.max == 0 ? (
+                    {discount?.max == 0 && discount?.percent == 0 ? (
                       <Grid xs={12} spacing={1} container>
                         <Grid xs={9} item>
                           <TextField
