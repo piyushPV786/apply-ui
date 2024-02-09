c36cd06a7de5d6f81d1a5f29712a7ddb1e884de5
aayushim <aayushim@regenesys.net>
2024-02-09 20:16:08 +0530

    apply-558-validation-error

---
+++ src/components/lead/form/data/address.tsx
@@ -0,0 +1,9 @@
+export const AddressData = [
+  { name: 0, label: "Postal" },
+  { name: 1, label: "Residential" },
+  { name: "city", label: "City" },
+  { name: "country", label: "Country" },
+  { name: "state", label: "State" },
+  { name: "street", label: "Address" },
+  { name: "zipCode", label: "Pin Code / Zip Code" },
+];
