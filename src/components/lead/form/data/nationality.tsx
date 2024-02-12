bd191ea1244e304d86966ff0c5a447a544746f3f
sonalik <sonalik@regenesys.net>
2024-02-12 18:18:25 +0530

    APPLY-563-nationality-error-msg

---
+++ src/components/lead/form/data/nationality.tsx
@@ -0,0 +1,18 @@
+export const NationalityData = [
+  {
+    name: "nationalityStatus",
+    label: "Nationality Status",
+  },
+  {
+    name: "nationality",
+    label: "Nationality",
+  },
+  {
+    name: "identificationDocumentType",
+    label: "Identification Document Type",
+  },
+  {
+    name: "identificationNumber",
+    label: "Identification Number",
+  },
+];
