397e9e45b42c490075787803c22ee5f57570ee69
aayushim <aayushim@regenesys.net>
2024-07-25 20:12:30 +0530

    RMS-5824

--- src/components/documents/context/common.tsx
+++ src/components/documents/context/common.tsx
@@ -32,6 +32,10 @@ export const bursarryFeilds = {
   Phone: "BURSARYLETTERPhone",
 };
 
+export const actionType = {
+  submit: "submit",
+  draft: "Draft",
+};
 export const dashboardRedirectStatus = [
   CommonEnums?.RESUB_APP_DOC,
   CommonEnums?.BURSARY_LETTER_PEND,
